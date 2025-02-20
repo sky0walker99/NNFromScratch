c43fc1cda3b2b0004b7f1ff6fe5eb492c11ce7c9
Haroon <106879583+sky0walker99@users.noreply.github.com>
2025-02-21 00:44:00 +0530

    Docstring Update  and Added NN.ipynb

--- Python/NN.py
+++ Python/NN.py
@@ -1,27 +1,38 @@
 import numpy as np
 
-np.random.seed(0)
-
-# input data ( X )with 3 samples
-X = [[1,2.3,-2.6,3.4],
-    [2.1,3.1,5.3,6.2],
-    [3.2,1.4,-1.1,-2.1]] 
-
+np.random.seed(0) # For Reproducibility
 
 class Dense_layer:
     """
     A fully connected (dense) layer in a neural network.
 
-
     Initialization:
-
     1. When loading a trained model,saved weights and biases are used.
     2. For a new model, weights are randomly initialized in a small range for stable training. 
 
+    Parameters:
+    ---------
+    n_inputs : int 
+        Number of input featurs (or no of neurons in the previous layer).
+    
+    n_neurons : int 
+        Number of neurons in this layer.
+    
+    Example:
+    -------
+    >>>>
+    #Input data ( X ) with 3 samples and 4 features(inputs) per sample  - shape(3,4)
+    X = np.random.randn(3,4)
+    layer1 = Dense_layer(4,3)
+    layer2 = Dense_layer(3,5)
+    layer1.forward(X)
+    layer2.forward(layer1.output)
+    output = layer2.output
+    print(output)
     """
-    def __init__(self,n_inputs , n_neurons):
+    def __init__(self,n_inputs : int  , n_neurons : int ):
         self.weights = 0.1*np.random.randn(n_inputs , n_neurons)
         self.biases = np.zeros((1,n_neurons))
     def forward(self,inputs):
         self.output = np.dot(inputs,self.weights) + self.biases
-
+        return self.output
--- Python/notes.md
+++ Python/notes.md
@@ -8,6 +8,17 @@ Initialization:
 - randn uses a gaussian distribution bounded aroud zero but some values would be bigger so the weights are multiplied by 0.1.
 - Scaling by 0.1 ensures smaller initial values, preventing large activations.
 
-biases are initialized to 0 because they are learned during training.while in some scenarios setting them to zero would not be apt.
+biases are initialized to 0 because they are learned during training. while in some scenarios setting them to zero would not be apt.
 
- 
+Activation Function
+Why use one? why not just use weights and biases without activation function?
+Without a non linear activation function the output will always be linear and we cant use any non-linear data and train the NN. The Activation function introdues non-linearity into the network.
+
+The activation function is applied to the (inputs*weights + bias )
+    Z=XW+b
+    A=Activation(Z)
+
+RelU (if x is less than or equal to 0 then y is 0  , else if x > 0  y will be equal to x) Function
+ReLU(x)=max(0,x)
+Less Vanishing gradient problems compared to sigmoid function.
+by tweaking by the bias the activation func can be offsetted.
