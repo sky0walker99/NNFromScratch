55abb8427eec865edae5882f5604be7221491f27
ivanrj7j <ivanrj7j@gmail.com>
2025-02-22 20:44:37 +0530

    added some more activations

--- Python/activation.py
+++ Python/activation.py
@@ -3,7 +3,18 @@ import math
 
 e = math.e
 
-class ReLU:
+class Activation:
+    """
+    # Activation
+
+    Activation is an abstract class that is used to represent an activation function.
+
+    Activation class implements all the required method as a template for an activation function.
+    """
+    def forward(self, x):
+        raise NotImplementedError("Formward should be implemented by the child")
+    
+class ReLU(Activation):
     """
     ReLU (Rectified Linear Unit) is an activation function used in neural networks to introduce non-linearity into the model.
     
@@ -12,7 +23,7 @@ class ReLU:
         self.output = np.maximum(0,x)
         return self.output
     
-class SoftMax:
+class SoftMax(Activation):
     """
     1. Exponentiate the output values
     2. Then normalise them by dividing with the output of other neurons in layer.
@@ -25,4 +36,35 @@ class SoftMax:
         normalised_score = exp_values / np.sum(exp_values,keepdims=True,axis=1)
         self.output = normalised_score
         return self.output 
+    
+class Sigmoid(Activation):
+    """
+    Sigmoid function is an activation function used in neural networks to introduce non-linearity into the model.
+    Output in the range (0, 1)
+    """
+
+    def forward(self, x):
+        self.output = 1 / (1 + np.exp(-x))
+        return self.output
+    
+class Tanh(Activation):
+    """
+    Hyperbolic tangent function is an activation function used in neural networks to introduce non-linearity into the model.
+    Output in the range (-1, 1)
+    """
+    def forward(self, x):
+        self.output = np.tanh(x)
+        return self.output
+    
+class ELU(Activation):
+    """
+    Exponential Linear Unit (ELU) is an activation function used in neural networks to introduce non-linearity into the model.
+    Output in the range (0, infinity)
+    """
+    def __init__(self, alpha:float):
+        super().__init__()
+        self.alpha = alpha
+    def forward(self, x):
+        self.output = np.where(x >= 0, x, self.alpha * (np.exp(x) - 1))
+        return self.output
         
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from Python.NN import Dense_layer
+from Python.activation import ReLU, SoftMax, Sigmoid, ELU, Tanh
 import numpy as np
 
 class DenseTest(unittest.TestCase):
@@ -26,6 +27,39 @@ class DenseTest(unittest.TestCase):
         
     def test_error(self):    
         self.assertRaises(ValueError, self.layer.forward, np.random.randn(30, 30))
+
+
+class TestActivationFunctions(unittest.TestCase):
+    def test_relu(self):
+        relu = ReLU()
+        x = np.array([-2, -1, 0, 1, 2])
+        expected = np.array([0, 0, 0, 1, 2])
+        np.testing.assert_array_equal(relu.forward(x), expected)
+    
+    def test_softmax(self):
+        softmax = SoftMax()
+        x = np.array([[1, 2, 3], [1, 2, 3]])
+        output = softmax.forward(x)
+        self.assertTrue(np.allclose(np.sum(output, axis=1), np.ones(output.shape[0])))
+    
+    def test_sigmoid(self):
+        sigmoid = Sigmoid()
+        x = np.array([-1, 0, 1])
+        expected = 1 / (1 + np.exp(-x))
+        np.testing.assert_array_almost_equal(sigmoid.forward(x), expected)
+    
+    def test_tanh(self):
+        tanh = Tanh()
+        x = np.array([-1, 0, 1])
+        expected = np.tanh(x)
+        np.testing.assert_array_almost_equal(tanh.forward(x), expected)
+    
+    def test_elu(self):
+        elu = ELU(alpha=1.0)
+        x = np.array([-1, 0, 1])
+        expected = np.where(x >= 0, x, 1.0 * (np.exp(x) - 1))
+        np.testing.assert_array_almost_equal(elu.forward(x), expected)
+
 
 
 if __name__ == "__main__":
